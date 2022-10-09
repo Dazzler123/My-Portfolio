55c0150c3f8216c2c1252eeab5a6c2d6be410783
Dazzler123 <dasinduhewagamage@gmail.com>
2022-10-10 01:38:09 +0530

    Set item details in place order form added

--- assignments/js/spa/pos/controller/PlaceOrderController.js
+++ assignments/js/spa/pos/controller/PlaceOrderController.js
@@ -3,6 +3,8 @@
 function loadAllCustomerIds() {
     //clear combo box
     $('#cbxSelectCustID').empty();
+    $('#cbxSelectCustID').append("<option>None</option>");
+
     for (let cus of customerArr) {
         $('#cbxSelectCustID').append("<option>" + cus.id + "</option>");
     }
@@ -12,7 +14,20 @@ function loadAllCustomerIds() {
 function loadAllItemCodes() {
     //clear combo box
     $('#cbxSelectItemCode').empty();
+    $('#cbxSelectItemCode').append("<option>None</option>");
+
     for (let itm of itemArr) {
         $('#cbxSelectItemCode').append("<option>" + itm.id + "</option>");
     }
 }
+
+//set item details to the textfields
+$('#cbxSelectItemCode').change( function () {
+    //clear before adding
+    $('#txtItemName,#txtAvailableQTYOnHand,#txtUnitPrice').val("");
+    //search item
+    var item = searchItemByID($(this).val());
+    $('#txtItemName').val(item.name);
+    $('#txtAvailableQTYOnHand').val(item.qty_On_Hand);
+    $('#txtUnitPrice').val(item.price_per_unit);
+});
