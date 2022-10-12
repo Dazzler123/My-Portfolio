46d324ec0a6235b03c5f9e874b67c10b104749d4
Dazzler123 <dasinduhewagamage@gmail.com>
2022-10-13 04:03:48 +0530

    Select table row function added

--- assignments/js/spa/pos/controller/ManageOrdersController.js
+++ assignments/js/spa/pos/controller/ManageOrdersController.js
@@ -25,7 +25,45 @@ $('#btnSearchOrder').click(function () {
                 $('#tbl_Orders_Body').append(row);
             }
         }
+        //select row
+        getOrderTblRowData();
     } else {
         alert("No such order found!");
     }
 });
+
+//get selected table row data
+function getOrderTblRowData() {
+    $('#tbl_Orders_Body > tr').click(function () {
+        let itmCode = $(this, '#tbl_Orders_Body>tr').children(':nth-child(1)').text();
+        let itmName = $(this, '#tbl_Orders_Body>tr').children(':nth-child(2)').text();
+        let qtyBght = $(this, '#tbl_Orders_Body>tr').children(':nth-child(4)').text();
+
+        // enable update delete btns
+        $('#btnUpdateItemQty').removeAttr('disabled');
+        $('#btnDeleteItemFromOrder').removeAttr('disabled');
+
+        //set data to update item qty bought modal textfields
+        $('#btnUpdateItemQty').click(function () {
+            //search before adding
+            if (searchItemByID(itmCode) != null) {
+                $('#txtUpdateOrderQty').val(qtyBght);
+            } else {
+                $("#txtUpdateOrderQty").val("");
+            }
+        });
+
+        //set data to delete item from order modal confirmation
+        $('#btnDeleteItemFromOrder').click(function () {
+            //search before adding
+            if (searchItemByID(itmCode) != null) {
+                $('#lbl_Order_Remove_Item_Code').text(itmCode);
+                $('#lbl_Order_Remove_Item_Name').text(itmName);
+            } else {
+                $('#lbl_Order_Remove_Item_Code').text("");
+                $('#lbl_Order_Remove_Item_Name').text("");
+            }
+        });
+    });
+}
+
